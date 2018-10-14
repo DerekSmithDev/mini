5fc9c77ccbde6aa19700405084ba4a393551dd7e
dereksmith83 <mdsmith1983@gmail.com>
2018-10-14 12:06:27 -0500

    one product w/params

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,4 +4,9 @@ class Api::ProductsController < ApplicationController
     render "all_products.json.jbuilder"
   end
 
+  def one_product
+    input_id = params["id"]
+    @product = Product.find_by(id: input_id)
+    render "one_product.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     get"/all_products" => "products#all_products"
+    get"/one_products" => "products#one_products"
+    get"/one_products/:id" => "products#one_products"
   end
 end
