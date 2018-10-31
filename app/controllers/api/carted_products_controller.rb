d1b6b8dd63c9842f962440e036fb1838e6ccbc60
dereksmith83 <mdsmith1983@gmail.com>
2018-10-30 20:21:21 -0500

    refactor

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -3,7 +3,6 @@ class Api::CartedProductsController < ApplicationController
   def index
 #    @carted_products = CartedProduct.where(status: "carted")
     @carted_products = current_user.carted_products.where(status: "carted")
-
     render "index.json.jbuilder"
   end
 
@@ -21,14 +20,15 @@ class Api::CartedProductsController < ApplicationController
   end
 
   def destroy
-    input_id = params["id"]
-    carted_product = current_user.carted_products.find_by(id: input_id)
-    carted_product.status = "removed"
+    input_id = params[:id]
+    @carted_product = current_user.carted_products.find_by(id: input_id)
+    @carted_product.status = "removed"
 
-    if carted_product.save
-      render json: {message: 'Cart_Product successfully destroyed'}, status: :created
+    if @carted_product.save
+      @success = 'Product #'+"#{@carted_product.id}"+' Successfully Removed from Your Cart '+"#{current_user.name}"
+      render json: {Cart: @success}, status: :gone
     else
-      render json: {errors: carted_product.errors.full_messages}, status: :bad_request
+      render json: {errors: @carted_product.errors.full_messages}, status: :bad_request
     end
   end
 end
